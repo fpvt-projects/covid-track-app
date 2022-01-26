1d787944d826382ee0f71a8fb89788bb87752e90
jerco2 <jericogodoy7296@gmail.com>
2022-01-26 14:12:18 +0800

    Added registration route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+  const loginRedirect = () => {
+    if (loggedIn == false) {
+      navigate("/sign-up");
+    }
+  };
+  useEffect(() => {
+    loginRedirect();
+  }, []);
+
   return (
     <div>
       <h1 class="text-lg font-bold">Hello World!</h1>
