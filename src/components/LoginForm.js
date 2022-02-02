de1aefef4ee3e86c57736b9ae7a9909112e94171
jerco2 <jericogodoy7296@gmail.com>
2022-02-02 12:29:17 +0800

    Added useEffect to remove error message and style for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
@@ -22,6 +22,18 @@ function LoginForm() {
       navigate("/");
     }
   };
+
+  useEffect(() => {
+    if (email !== null || email !== "") {
+      setError("");
+    }
+  }, [email]);
+
+  useEffect(() => {
+    if (password !== null || password !== "") {
+      setError("");
+    }
+  }, [password]);
 
   return (
     <div
