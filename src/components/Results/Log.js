3901286c8e0da65a6cfd32bf38d2a3df277995c6
jerco2 <jericogodoy7296@gmail.com>
2022-02-05 12:10:20 +0800

    Redesign result logs added spin animate on status icon

--- src/components/Results/Log.js
+++ src/components/Results/Log.js
@@ -3,21 +3,26 @@ import ResultInfo from "./ResultInfo";
 
 function Log({ date, status, results, count }) {
   const [toggleInfo, setToggleInfo] = useState(false);
+  const [toggleAnimatee, setToggleAnimate] = useState(false);
 
-  const handleClick = () => setToggleInfo(!toggleInfo);
+  const handleClick = () => {
+    setToggleInfo(!toggleInfo);
+    setToggleAnimate(!toggleAnimatee);
+  };
 
   return (
     <div
       className={`${
-        count % 2 === 0 ? "bg-gray-200" : "bg-gray-300"
-      } cursor-pointer hover:bg-gray-400 select-none`}
+        count % 2 === 0 ? "bg-gray-200" : "bg-gray-300 "
+      } cursor-pointer hover:bg-gray-100 select-none`}
       onClick={handleClick}
     >
-      <div className={`h-12 px-2 flex items-center`}>
+      <div className={`h-12 px-2 flex items-center `}>
         <div
-          className={`w-3 h-3 rounded-2xl mx-2 ${
+          className={`w-3 h-3 rounded-sm mx-2 ${
             status === "Ongoing" ? "bg-red-500" : "bg-green-500"
-          }`}
+          }
+          ${toggleAnimatee ? "animate-spin" : "animate-none"}`}
         ></div>
         <div className={`w-28 text-sm font-semibold tracking-widest`}>
           {date}
